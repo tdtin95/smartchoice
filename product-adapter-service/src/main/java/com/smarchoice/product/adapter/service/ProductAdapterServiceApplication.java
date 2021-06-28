c9290130bf9e2d6e8f829e4cb8b2aaf74887afdb
Tin Tran <tdtin95@gmail.com>
2021-06-28 22:07:59 +0700

    move services config to spring cloud config

--- product-adapter-service/src/main/java/com/smarchoice/product/adapter/service/ProductAdapterServiceApplication.java
+++ product-adapter-service/src/main/java/com/smarchoice/product/adapter/service/ProductAdapterServiceApplication.java
@@ -2,12 +2,14 @@ package com.smarchoice.product.adapter.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class ProductAdapterServiceApplication {
 
     public static void main(String[] args) {
